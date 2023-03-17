d2219011732df01b556dc850821e13c361e67489
Vishalpateldodla <117738669+Vishalpateldodla@users.noreply.github.com>
2023-03-17 11:46:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-window.promises = [];
-
-// Do not change the code above this
-// add your promises to the array `promises`
-for (let i = 0; i < 5; i++) {
-const randomTime = Math.floor(Math.random() * 5) + 1; // random time between 1 and 5 seconds
-const promise = new Promise((resolve) => {
-setTimeout(() => {
-resolve(randomTime);
-}, randomTime * 1000); // convert to milliseconds
-});
-window.promises.push(promise);
-}
-
-Promise.any(window.promises)
-.then((result) => {
-const outputDiv = document.getElementById("output");
-outputDiv.innerText = `Promise resolved with result ${result}`;
-})
-.catch((error) => {
-console.log(error);
+// create an array of 5 promises that resolve with a random time between 1 and 5 seconds
+const promises = Array.from({ length: 5 }, () => {
+  const randomTime = Math.floor(Math.random() * 5) + 1;
+  return new Promise(resolve => {
+    setTimeout(() => resolve(randomTime), randomTime * 1000);
+  });
 });
 
-
-
-
-
+// use Promise.any to wait for the first promise to resolve, and print its result to the output div
+Promise.any(promises)
+  .then(result => {
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerText = `The first promise resolved with result: ${result}`;
+  })
+  .catch(error => console.error(error));
