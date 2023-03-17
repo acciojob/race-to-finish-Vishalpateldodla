785d9723851b2170c617096d0d05bd4d6bd23632
Vishalpateldodla <117738669+Vishalpateldodla@users.noreply.github.com>
2023-03-17 11:37:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+for (let i = 0; i < 5; i++) {
+const randomTime = Math.floor(Math.random() * 5) + 1; // random time between 1 and 5 seconds
+const promise = new Promise((resolve) => {
+setTimeout(() => {
+resolve(randomTime);
+}, randomTime * 1000); // convert to milliseconds
+});
+window.promises.push(promise);
+}
+
+Promise.any(window.promises)
+.then((result) => {
+const outputDiv = document.getElementById("output");
+outputDiv.innerText = `Promise resolved with result ${result}`;
+})
+.catch((error) => {
+console.log(error);
+});
+
+
+
+
+
